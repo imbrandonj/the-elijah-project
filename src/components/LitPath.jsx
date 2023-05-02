1019fbda2945d990af4bcdf4e40d98ad8e7a754e
Brandon <branj.dev@gmail.com>
2023-05-02 00:12:44 -0500

    introduced emojis, lit set 4, css styles

--- src/components/LitPath.jsx
+++ src/components/LitPath.jsx
@@ -18,59 +18,67 @@ export default function LitPath() {
   // problem object sets `question` & `answer` properties depending on the state (the problemSet)
   let problem = generateProblem(problemSet);
 
-  // Return for the first 3 problem sets
-  if (problemSet <= 3) {
-    return (
-      <div>
-        <RocketHeader />
+  return (
+    <div>
+      <RocketHeader />
 
-        <div id="litPath">
-          <div id="litWrapper">
-            <header className="setHeader">
-              Literacy: {setHeader[problemSet + 1]}
-            </header>
+      <div id="litPath">
+        <div id="litWrapper">
+          <header className="setHeader">
+            Literacy: {setHeader[problemSet - 1]}
+          </header>
+          {problemSet <= 3 ? (
             <p id="litQ">
-              Enter the letter:
+              <span class="lineHeight4">Enter the letter:</span>
               <br />
-              <span id="focused">{problem.question}</span>
+              <span id="letterQ">{problem.question}</span>
             </p>
-            <input
-              id="litAns"
-              type="text"
-              onKeyDown={(event) => {
-                // listen for enter keydown
-                if (event.key === "Enter") {
-                  // set the value of the user's answer
-                  let inputValue = event.target.value;
+          ) : (
+            <p id="litQ4">
+              <span class="lineHeight8">Spell the word:</span>
+              <br />
+              <span class="medEmoji">{problem.emoji}</span>
+              <br />
+              <span class="emojiFont">{problem.question}</span>
+            </p>
+          )}
 
-                  // first 2 problem sets are case insensitive
-                  if (problemSet === 0) {
-                    inputValue = inputValue.toUpperCase();
-                  } else if (problemSet === 1) {
-                    inputValue = inputValue.toLowerCase();
-                  }
+          <input
+            id="litAns"
+            type="text"
+            onKeyDown={(event) => {
+              // listen for enter keydown
+              if (event.key === "Enter") {
+                // set the value of the user's answer
+                let inputValue = event.target.value;
 
-                  event.target.value = ""; // clears the input box
-
-                  // module answerEvent.js
-                  answerEvent(
-                    event, // enter keydown event
-                    inputValue, // the user's given answer
-                    problem.answer, // the correct answer
-                    correctTally, // total correct tally (state)
-                    setCorrectTally, // to set total correct tally (state)
-                    problemSet, // current problem set (state)
-                    setProblemSet // to increment the problem set (state) if correct >= 20
-                  );
+                // first 2 problem sets are case insensitive
+                if (problemSet === 1) {
+                  inputValue = inputValue.toUpperCase();
+                } else if (problemSet === 2) {
+                  inputValue = inputValue.toLowerCase();
                 }
-              }}
-            />
-            <Footbox correct={correctTally} style={"litFill"} />
-          </div>
+
+                event.target.value = ""; // clears the input box
+
+                // module answerEvent.js
+                answerEvent(
+                  event, // enter keydown event
+                  inputValue, // the user's given answer
+                  problem.answer, // the correct answer
+                  correctTally, // total correct tally (state)
+                  setCorrectTally, // to set total correct tally (state)
+                  problemSet, // current problem set (state)
+                  setProblemSet // to increment the problem set (state) if correct >= 20
+                );
+              }
+            }}
+          />
+          <Footbox correct={correctTally} style={"litFill"} />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 // Problem set headers
--- src/modules/litProblems.js
+++ src/modules/litProblems.js
@@ -6,10 +6,36 @@ export default function generateProblem(problemSet) {
   const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
   const mixLetters = upperLetters + lowerLetters;
 
+  // problem set 4
+  const litSet4 = [
+    { animal: "cow", emoji: "🐄" },
+    { animal: "cat", emoji: "🐈" },
+    { animal: "dog", emoji: "🐕" },
+    { animal: "tiger", emoji: "🐅" },
+    { animal: "horse", emoji: "🐎" },
+    { animal: "pig", emoji: "🐖" },
+    { animal: "mouse", emoji: "🐁" },
+    { animal: "duck", emoji: "🦆" },
+    { animal: "owl", emoji: "🦉" },
+    { animal: "frog", emoji: "🐸" },
+    { animal: "snake", emoji: "🐍" },
+    { animal: "shark", emoji: "🦈" },
+    { animal: "snail", emoji: "🐌" },
+    { animal: "spider", emoji: "🕷" },
+    { animal: "bee", emoji: "🐝" },
+    { animal: "sloth", emoji: "🦥" },
+    { animal: "skunk", emoji: "🦨" },
+    { animal: "bear", emoji: "🐻" },
+    { animal: "lion", emoji: "🦁" },
+    { animal: "fox", emoji: "🦊" },
+    { animal: "wolf", emoji: "🐺" },
+  ];
+
   // problem object sets question & answer depending on the state (the problemSet)
   let problem = {
     question: "",
     answer: "",
+    emoji: "", // relevent for problem set 4
   };
 
   // generate problem depending on problemSet (state)
@@ -23,6 +49,11 @@ export default function generateProblem(problemSet) {
   } else if (problemSet === 3) {
     problem.question = mixLetters.charAt(randomNum(mixLetters.length));
     problem.answer = problem.question;
+  } else if (problemSet === 4) {
+    let randomSelect = randomNum(litSet4.length);
+    problem.question = litSet4[randomSelect].animal;
+    problem.answer = litSet4[randomSelect].animal;
+    problem.emoji = litSet4[randomSelect].emoji;
   }
 
   return problem;
