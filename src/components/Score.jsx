75e8d9b62705157e2eb5272852f6a7bc87dd9408
Brandon <imbrandonj42@gmail.com>
2023-10-23 17:32:38 -0500

    implements `Score.jsx`

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,6 +1,9 @@
+import astronaut from '@root/assets/svgs/astronaut.svg';
+
 export default function Score({ score }) {
   return (
     <h3 id="score">
+      <img src={astronaut} height={50} />
       Player Score: <span>{score}</span>
     </h3>
   );
