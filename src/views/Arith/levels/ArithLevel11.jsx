b6c972183fb662e1a52c9b2325298694549df182
Brandon <imbrandonj42@gmail.com>
2024-02-27 13:36:53 -0600

    introduces new `ArithLevel11.jsx` & `ArithLevel12.jsx`

--- src/views/Arith/levels/ArithLevel11.jsx
+++ src/views/Arith/levels/ArithLevel11.jsx
@@ -5,17 +5,25 @@ import Timer from '@root/components/Timer/Timer.jsx';
 import Tipbox from '@root/components/Tipbox/Tipbox.jsx';
 
 // imported internal modules:
-import { level11 } from './arithProblems.js';
-import generateProblem from '@root/modules/generateProblem.js';
+import { getAudio } from './arithAudio.js';
+import { randomNum } from '@root/modules/util.js';
+import speakText from '@root/modules/speakText.js';
 import tallyUp from '@root/modules/tallyUp.js';
 
 import '../Arith.css';
 
 // imported hooks:
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 
 /*
     Arith Level 11 Component
+
+    This level uses 4 input boxes for an operation
+    Format: a (+ or -) b = c
+    Where the user must input the numbers listed from the given audio (a, b, c)
+    and type the correct operation type (+ or -)
+
+    The audio is imported from arithAudio.js
 
     `setLevelEvent` prop (state) is passed from Arith view
       - if true, renders a <LevelUp /> component to display from Arith view
@@ -25,61 +33,157 @@ export default function ArithLevel11({
   levelScore,
   setLevelScore,
 }) {
-  const problemHistory = useRef([]); // to store problem history
+  const [operand1, setOperand1] = useState(0); // defaults to 0 before knowing if operation is 'add' or 'sub'
+  const [operand2, setOperand2] = useState(0); // defaults to 0 before knowing if operation is 'add' or 'sub'
+  const [answer, setAnswer] = useState(0); // defaults to 0 before knowing if operation is 'add' or 'sub'
+  const [operation, setOperation] = useState(''); // operation is either addition or subtraction
   const [correctTally, setCorrectTally] = useState(0); // total correct tally
 
-  const problemSet = level11; // imported problems for this level
+  // user provided input:
+  const [userOperand1, setUserOperand1] = useState(''); // default=displays empty input box
+  const [userOperand2, setUserOperand2] = useState(''); // default=displays empty input box
+  const [userAnswer, setUserAnswer] = useState(''); // default=displays empty input box
 
-  // `problem` is an object with `question` & `answer` properties
-  const [problem, setProblem] = useState(
-    () => generateProblem(problemSet, problemHistory, true) // generate a unique problem
-  );
+  let audio; // global, set in useEffect hook but also called in playButton()
 
-  // focuses on input box with each render
+  // must determine if the operation is addition or subtraction
+  // then set the component state appropriately:
   useEffect(() => {
-    document.getElementById('mathAns').focus();
-  }, []);
+    const isAddition = Math.random() < 0.5; // boolean logic for 50% chance
+    setOperation(isAddition ? 'add' : 'sub');
+
+    // generate operands based on operation type:
+    let num1, num2;
+    if (isAddition) {
+      num1 = randomNum(5);
+      num2 = randomNum(5);
+      setOperand1(num1);
+      setOperand2(num2);
+    } else {
+      num1 = randomNum(9) + 1;
+      num2 = randomNum(num1 - 1);
+      setOperand1(num1);
+      setOperand2(num2);
+    }
+
+    setAnswer(isAddition ? num1 + num2 : num1 - num2);
+
+    // Calculate audio based on operation type and operands
+    audio = getAudio(operation, num1, num2);
+
+    // Play audio and focus the input box
+    const mp3 = new Audio(audio);
+    mp3.play();
+    document.getElementById('operand1').focus();
+  }, [correctTally, operation]);
+
+  // audio plays on component render
+  // and `operand1` input box becomes focus
+  // useEffect(() => {
+  //   let audio = getAudio(operation, operand1, operand2);
+
+  //   const mp3 = new Audio(audio);
+  //   mp3.play(); // play on problem load
+  //   document.getElementById('operand1').focus();
+  // }, [correctTally]);
+
+  // audio play button:
+  const playButton = () => {
+    const mp3 = new Audio(audio);
+    console.log('test from play button');
+    console.log(audio);
+    mp3.play();
+  };
+
+  // check answer event:
+  function answerEvent() {
+    // correct answer event:
+    if (
+      operand1 === userOperand1 &&
+      operand2 === userOperand2 &&
+      answer === Number(userAnswer)
+    ) {
+      console.log(true);
+
+      setUserOperand1('');
+      setUserOperand2('');
+      setUserAnswer('');
+
+      // module tallyUp.js
+      tallyUp(
+        20, // `totalQuestions`
+        correctTally, // total correct tally (state)
+        setCorrectTally, // to set total correct tally (set state)
+        setLevelUpEvent // to set a level up event and display `LevelUp` component on rerender (set state)
+      );
+
+      setLevelScore(levelScore + 20);
+
+      // reset the incorrect numbers, keep the correct ones:
+    } else {
+      if (operand1 != userOperand1) setUserOperand1('');
+      if (operand2 != userOperand2) setUserOperand2('');
+      if (answer != Number(userAnswer)) setUserAnswer(' ');
+      console.log(false);
+    }
+  }
 
   return (
     <div id="ArithLevel">
-      <LevelHeader text="Add the two numbers together." score={levelScore} />
-      <div id="mathQABundle">
-        <p id="mathQ">{problem.question}</p>
+      <LevelHeader text="Type numbers of an operation" score={levelScore} />
+      <button className="speaker" onClick={playButton}>
+        🔊
+      </button>
+      <div id="mathQABundle2">
+        <p id="mathQ">
+          <input
+            id="operand1"
+            className="operandBox"
+            type="text"
+            value={userOperand1}
+            onChange={event => {
+              if (isNaN(parseInt(event.target.value))) setUserOperand1('');
+              else setUserOperand1(parseInt(event.target.value));
+            }}
+            onKeyDown={event => {
+              // listen for enter keydown
+              if (event.key === 'Enter') answerEvent();
+            }}
+          />{' '}
+          +{' '}
+          <input
+            id="operand2"
+            className="operandBox"
+            type="text"
+            value={userOperand2}
+            onChange={event => {
+              if (isNaN(parseInt(event.target.value))) setUserOperand2('');
+              else setUserOperand2(parseInt(event.target.value));
+            }}
+            onKeyDown={event => {
+              // listen for enter keydown
+              if (event.key === 'Enter') answerEvent();
+            }}
+          />{' '}
+          =
+        </p>
         <input
           id="mathAns"
           type="text"
+          value={userAnswer}
+          onChange={event => {
+            if (isNaN(parseInt(event.target.value))) setUserAnswer('');
+            else setUserAnswer(parseInt(event.target.value));
+          }}
           onKeyDown={event => {
             // listen for enter keydown
-            if (event.key === 'Enter') {
-              // set the value of the user's answer
-              let inputValue = parseInt(event.target.value); // math answers must be parsed to int
-
-              event.target.value = ''; // clears the input box
-
-              // correct answer event:
-              if (inputValue === problem.answer) {
-                // module tallyUp.js
-                tallyUp(
-                  20, // `totalQuestions`
-                  correctTally, // total correct tally (state)
-                  setCorrectTally, // to set total correct tally (set state)
-                  setLevelUpEvent // to set a level up event and display `LevelUp` component on rerender (set state)
-                );
-
-                setLevelScore(levelScore + 20);
-
-                // generate a new problem after processing the current one
-                setProblem(generateProblem(problemSet, problemHistory, true)); // generate a unique problem
-              } else if (levelScore >= 10) {
-                setLevelScore(levelScore - 10);
-              }
-            }
+            if (event.key === 'Enter') answerEvent();
           }}
         />
       </div>
       <Footbox correct={correctTally} />
       <Timer />
-      <Tipbox text="Tip: You can still use your fingers to count. 🖐️🖐️ equals 10. Close your fingers, and every finger raised from there is 11-20." />
+      <Tipbox text="Tip: Click the speaker to hear the operation. Press enter to submit the operation." />
     </div>
   );
 }
---
+++ src/views/Arith/levels/ArithLevel12.jsx
@@ -0,0 +1,87 @@
+// imported internal components:
+import LevelHeader from '@root/components/LevelHeader/LevelHeader.jsx';
+import Footbox from '@root/components/Footbox/Footbox.jsx';
+import Timer from '@root/components/Timer/Timer.jsx';
+import Tipbox from '@root/components/Tipbox/Tipbox.jsx';
+
+// imported internal modules:
+import { level12 } from './arithProblems.js';
+import generateProblem from '@root/modules/generateProblem.js';
+import tallyUp from '@root/modules/tallyUp.js';
+
+import '../Arith.css';
+
+// imported hooks:
+import { useState, useEffect, useRef } from 'react';
+
+/*
+    Arith Level 12 Component
+
+    addition { 0-20 + 0-20 = 11-20 }
+
+    `setLevelEvent` prop (state) is passed from Arith view
+      - if true, renders a <LevelUp /> component to display from Arith view
+*/
+export default function ArithLevel12({
+  setLevelUpEvent,
+  levelScore,
+  setLevelScore,
+}) {
+  const problemHistory = useRef([]); // to store problem history
+  const [correctTally, setCorrectTally] = useState(0); // total correct tally
+
+  const problemSet = level12; // imported problems for this level
+
+  // `problem` is an object with `question` & `answer` properties
+  const [problem, setProblem] = useState(
+    () => generateProblem(problemSet, problemHistory, true) // generate a unique problem
+  );
+
+  // focuses on input box with each render
+  useEffect(() => {
+    document.getElementById('mathAns').focus();
+  }, []);
+
+  return (
+    <div id="ArithLevel">
+      <LevelHeader text="Add the two numbers together." score={levelScore} />
+      <div id="mathQABundle">
+        <p id="mathQ">{problem.question}</p>
+        <input
+          id="mathAns"
+          type="text"
+          onKeyDown={event => {
+            // listen for enter keydown
+            if (event.key === 'Enter') {
+              // set the value of the user's answer
+              let inputValue = parseInt(event.target.value); // math answers must be parsed to int
+
+              event.target.value = ''; // clears the input box
+
+              // correct answer event:
+              if (inputValue === problem.answer) {
+                // module tallyUp.js
+                tallyUp(
+                  20, // `totalQuestions`
+                  correctTally, // total correct tally (state)
+                  setCorrectTally, // to set total correct tally (set state)
+                  setLevelUpEvent // to set a level up event and display `LevelUp` component on rerender (set state)
+                );
+
+                setLevelScore(levelScore + 20);
+
+                // generate a new problem after processing the current one
+                setProblem(generateProblem(problemSet, problemHistory, true)); // generate a unique problem
+              } else if (levelScore >= 10) {
+                setLevelScore(levelScore - 10);
+              }
+            }
+          }}
+        />
+      </div>
+      <Footbox correct={correctTally} />
+      <Timer />
+      <Tipbox text="Tip: You can still use your fingers to count. 🖐️🖐️ equals 10. Close your fingers, and every finger raised from there is 11-20." />
+    </div>
+  );
+}
