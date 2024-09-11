24c86ecfa028d42bbef8e07c800025edaa5144fa
Brandon <imbrandonj42@gmail.com>
2024-09-11 18:01:33 -0500

    adds `Swiper` arrows

--- src/views/Dashboard/DashEntry.jsx
+++ src/views/Dashboard/DashEntry.jsx
@@ -1,8 +1,9 @@
 // Swiper imports for carousel
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCoverflow, Keyboard } from 'swiper/modules';
+import { EffectCoverflow, Keyboard, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
+import 'swiper/css/navigation';
 
 import { useView } from '@root/contexts/ViewContext.jsx'; // global state
 
@@ -26,57 +27,66 @@ export default function DashEntry({ setDashSelect }) {
   const redirectConfig = () => setDashSelect('config');
   const redirectHome = () => setView('MainMenu');
   return (
-    <div id="dashSwiper">
+    <div id="dashSwiper" className="flex-col align-center">
       <h2>Select an action:</h2>
-      <Swiper
-        effect={'coverflow'}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 30,
-          depth: 100,
-          modifier: 1,
-          slideShadows: false,
-        }}
-        loop={true}
-        centeredSlides={true}
-        slidesPerView={2}
-        grabCursor={true}
-        keyboard={{
-          enabled: true,
-        }}
-        modules={[EffectCoverflow, Keyboard]}
-      >
-        <SwiperSlide>
-          <button
-            onClick={redirectBegin}
-            className="dashSwiperBtn flex flex-col align-center"
-          >
-            <img src={RocketLaunch} height={160} />
-            Launch Mission
-          </button>
-        </SwiperSlide>
-        <SwiperSlide>
-          <button className="dashSwiperBtn flex flex-col align-center">
-            <img src={PlanetFlag} height={160} />
-            Player Statistics
-          </button>
-        </SwiperSlide>
-        <SwiperSlide>
-          <button className="dashSwiperBtn flex flex-col align-center">
-            <img src={FloatAstro} height={120} />
-            Profile Configuration
-          </button>
-        </SwiperSlide>
-        <SwiperSlide>
-          <button
-            className="dashSwiperBtn flex flex-col align-center"
-            onClick={redirectHome}
-          >
-            <img src={Stars} height={140} />
-            Sign Out
-          </button>
-        </SwiperSlide>
-      </Swiper>
+      <hr />
+      <div id="swiperWrapper">
+        <div className="swiper-button-prev"></div>
+        <Swiper
+          effect={'coverflow'}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 60,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          loop={true}
+          centeredSlides={true}
+          slidesPerView={2}
+          grabCursor={true}
+          keyboard={{
+            enabled: true,
+          }}
+          navigation={{
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          }}
+          modules={[EffectCoverflow, Keyboard, Navigation]}
+        >
+          <SwiperSlide>
+            <button
+              onClick={redirectBegin}
+              className="dashSwiperBtn flex-col align-center justify-center"
+            >
+              <img src={RocketLaunch} height={160} />
+              Launch Mission
+            </button>
+          </SwiperSlide>
+          <SwiperSlide>
+            <button className="dashSwiperBtn flex-col align-center justify-center">
+              <img src={PlanetFlag} height={160} />
+              Player Statistics
+            </button>
+          </SwiperSlide>
+          <SwiperSlide>
+            <button className="dashSwiperBtn flex-col align-center justify-center">
+              <img src={FloatAstro} height={120} />
+              Profile Configuration
+            </button>
+          </SwiperSlide>
+          <SwiperSlide>
+            <button
+              className="dashSwiperBtn flex-col align-center justify-center"
+              onClick={redirectHome}
+            >
+              <img src={Stars} height={160} />
+              Sign Out
+            </button>
+          </SwiperSlide>
+        </Swiper>
+        <div className="swiper-button-next"></div>
+      </div>
     </div>
   );
 }
--- src/views/Dashboard/SelectMission.jsx
+++ src/views/Dashboard/SelectMission.jsx
@@ -1,6 +1,6 @@
 // Swiper imports for carousel
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCoverflow, Keyboard } from 'swiper/modules';
+import { EffectCoverflow, Keyboard, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 
@@ -29,64 +29,73 @@ export default function SelectMission({ setMissionSelect, setDashSelect }) {
   const handleReturn = () => setDashSelect('entry');
 
   return (
-    <div id="dashSwiper">
+    <div id="dashSwiper" className="flex-col align-center">
       <h2>Select a planet for exploration:</h2>
-      <Swiper
-        effect={'coverflow'}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 30,
-          depth: 100,
-          modifier: 1,
-          slideShadows: false,
-        }}
-        loop={true}
-        centeredSlides={true}
-        slidesPerView={2}
-        grabCursor={true}
-        keyboard={{
-          enabled: true,
-        }}
-        modules={[EffectCoverflow, Keyboard]}
-      >
-        <SwiperSlide>
-          <button
-            onClick={alphaSelect}
-            className="dashSwiperBtn flex-col align-center alphaLitColor"
-          >
-            <img src={AlphaLiteracy} height={160} />
-            Alpha-Literacy
-          </button>
-        </SwiperSlide>
-        <SwiperSlide>
-          <button
-            id="arith"
-            onClick={arithSelect}
-            className="dashSwiperBtn flex-col align-center arithColor"
-          >
-            <img src={Arith} height={160} />
-            Arith
-          </button>
-        </SwiperSlide>
-        <SwiperSlide>
-          <button
-            onClick={perspectiveSelect}
-            className="dashSwiperBtn flex-col align-center perspectiveColor"
-          >
-            <img src={Perspective} height={160} />
-            Perspective
-          </button>
-        </SwiperSlide>
-        <SwiperSlide>
-          <button
-            onClick={handleReturn}
-            className="dashSwiperBtn flex-col align-center"
-          >
-            <img src={RocketHome} height={140} />
-            Return to Base
-          </button>
-        </SwiperSlide>
-      </Swiper>
+      <hr />
+      <div id="swiperWrapper">
+        <div className="swiper-button-next"></div>
+        <Swiper
+          effect={'coverflow'}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 30,
+            depth: 100,
+            modifier: 1,
+            slideShadows: false,
+          }}
+          loop={true}
+          centeredSlides={true}
+          slidesPerView={2}
+          grabCursor={true}
+          keyboard={{
+            enabled: true,
+          }}
+          navigation={{
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          }}
+          modules={[EffectCoverflow, Keyboard, Navigation]}
+        >
+          <SwiperSlide>
+            <button
+              onClick={alphaSelect}
+              className="dashSwiperBtn flex-col align-center justify-center alphaLitColor"
+            >
+              <img src={AlphaLiteracy} height={160} />
+              Alpha-Literacy
+            </button>
+          </SwiperSlide>
+          <SwiperSlide>
+            <button
+              id="arith"
+              onClick={arithSelect}
+              className="dashSwiperBtn flex-col align-center justify-center arithColor"
+            >
+              <img src={Arith} height={160} />
+              Arith
+            </button>
+          </SwiperSlide>
+          <SwiperSlide>
+            <button
+              onClick={perspectiveSelect}
+              className="dashSwiperBtn flex-col align-center justify-center perspectiveColor"
+            >
+              <img src={Perspective} height={160} />
+              Perspective
+            </button>
+          </SwiperSlide>
+          <SwiperSlide>
+            <button
+              onClick={handleReturn}
+              className="dashSwiperBtn flex-col align-center justify-center"
+            >
+              <img src={RocketHome} height={160} />
+              Return to Base
+            </button>
+          </SwiperSlide>
+        </Swiper>
+        <div className="swiper-button-prev"></div>
+      </div>
     </div>
   );
 }
